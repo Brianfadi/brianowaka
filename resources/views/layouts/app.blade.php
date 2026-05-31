24be4acf3fbcae6ac30358113a9e660fd917e387
Brianfadi <brianfadi20@gmail.com>
2026-05-31 18:57:09 +0300

    Add Google Site Verification meta tag to all layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="google-site-verification" content="RqUWPzkRhAoeNUOEeBLFF9WwrIUOkIwbulF8ebikclo" />
 
         <title>{{ $settings['site_name'] ?? 'Brian Owaka' }} - @yield('title', 'Full Stack Developer')</title>
         
