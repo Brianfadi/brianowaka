5213a002e9d58a17fa90b10f008ef9bbc7d801e2
Brianfadi <brianfadi20@gmail.com>
2026-06-01 20:53:24 +0300

    Remove topbar completely

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,20 +37,5 @@ class AppServiceProvider extends ServiceProvider
                 $view->with('settings', $settings);
             }
         });
-
-        // Share topbar data (contact + social) with all views, independent of page-specific $settings
-        view()->composer('*', function ($view) {
-            $view->with('topbar', [
-                'contact_email' => \App\Models\Setting::get('contact_email', 'brian@brianowaka.com'),
-                'contact_phone' => \App\Models\Setting::get('contact_phone', '+254 712 345 678'),
-                'contact_location' => \App\Models\Setting::get('contact_location', 'Nairobi, Kenya'),
-                'social_github'    => \App\Models\Setting::get('social_github'),
-                'social_linkedin'  => \App\Models\Setting::get('social_linkedin'),
-                'social_twitter'   => \App\Models\Setting::get('social_twitter'),
-                'social_facebook'  => \App\Models\Setting::get('social_facebook'),
-                'social_instagram' => \App\Models\Setting::get('social_instagram'),
-                'social_youtube'   => \App\Models\Setting::get('social_youtube'),
-            ]);
-        });
     }
 }
